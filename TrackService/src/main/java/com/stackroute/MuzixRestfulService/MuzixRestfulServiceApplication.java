fb3a7588e3c67af994b76b2b54b7fe16ff7c8a6b
Akhila <kakhila96@gmail.com>
2019-05-29 12:34:10 +0530

    add TrackService.properties

--- TrackService/src/main/java/com/stackroute/MuzixRestfulService/MuzixRestfulServiceApplication.java
+++ TrackService/src/main/java/com/stackroute/MuzixRestfulService/MuzixRestfulServiceApplication.java
@@ -5,8 +5,11 @@ import com.stackroute.MuzixRestfulService.repository.TrackRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -29,3 +32,20 @@ public class MuzixRestfulServiceApplication {
 	}*/
 
 }
+
+@RefreshScope
+@RestController
+class MessageRestController {
+//
+//		@Value("${msg:Hello world - Config Server is not working..pelase check}")
+//		private String msg;
+
+	@RequestMapping
+	String getMsg() {
+		String msg;
+		msg = "stackroute";
+		return msg;
+
+	}
+}
+
--- UserService/src/main/java/com/stackroute/SpringBoot_Restful_Service/SpringBootRestfulServiceApplication.java
+++ UserService/src/main/java/com/stackroute/SpringBoot_Restful_Service/SpringBootRestfulServiceApplication.java
@@ -9,27 +9,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringBootRestfulServiceApplication {
-
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringBootRestfulServiceApplication.class, args);
+	//	System.out.println(msg);
 	}
 
+}
+	@RefreshScope
+	@RestController
+	class MessageRestController {
 //
-//	@RefreshScope
-//	@RestController
-//	class MessageRestController {
-//
-////		@Value("${msg:Hello world - Config Server is not working..pelase check}")
-////		private String msg;
-//
-//		@RequestMapping
-//		String getMsg() {
-//		String msg;
-//			msg="hello world";
-//			return msg;
-//
-//		}
+//		@Value("${msg:Hello world - Config Server is not working..pelase check}")
+//		private String msg;
 
+		@RequestMapping
+		String getMsg() {
+			String msg;
+			msg = "hello world";
+			return msg;
+
+		}
 	}
+
 
