a5c0bdf8f657fcb5af140fe1137c49ef42d31220
Arathi <mj.arathi@gmail.com>
2019-05-30 10:50:05 +0530

    update

--- UserService/src/main/java/com/stackroute/SpringBoot_Restful_Service/SpringBootRestfulServiceApplication.java
+++ UserService/src/main/java/com/stackroute/SpringBoot_Restful_Service/SpringBootRestfulServiceApplication.java
@@ -4,32 +4,19 @@ import lombok.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringBootRestfulServiceApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringBootRestfulServiceApplication.class, args);
-	//	System.out.println(msg);
 	}
 
 }
-	@RefreshScope
-	@RestController
-	class MessageRestController {
-//
-//		@Value("${msg:Hello world - Config Server is not working..pelase check}")
-//		private String msg;
 
-		@RequestMapping
-		String getMsg() {
-			String msg;
-			msg = "hello world";
-			return msg;
-
-		}
-	}
 
 
